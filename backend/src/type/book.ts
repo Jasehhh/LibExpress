77b6d6f2cc95302c4a3fda4e5e5529270e095cb8
Nelson Lago III <lagonelson77@gmail.com>
2026-09-26 17:31:09 +0800

    chore: update backend types for authors and member names

--- backend/src/type/book.ts
+++ backend/src/type/book.ts
@@ -1,8 +1,11 @@
+import { Author } from "./author";
+
 export interface Book {
   id: number;
   isbn: string;
   title: string;
-  author: string;
+  author_id: string;
+  author: Author;
   url: string | null;
   genre: BookGenre;
   total_copies: number;
